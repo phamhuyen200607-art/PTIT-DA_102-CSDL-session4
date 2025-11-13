c65a41eec0c95d32c2de212138cf157a054486ce
phamhuyen200607-art <phamhuyen200607@gmail.com>
2025-11-13 22:56:08 +0700

    Add files via upload

---
+++ cơ bản 3.s
@@ -0,0 +1,6 @@
+USE CompanyDB;
+INSERT INTO Employees(Firstname,Lastname,HeriDate,Salary) VALUES
+('Nguyễn','Văn An','2023-03-15',15000000),
+('Trần','Thị Út','2022-10-19',10000000),
+('Phạm','Văn Nghĩa','2020-09-10',30000000);
+SELECT * FROM Employees;
